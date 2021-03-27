ed86bf2be16e78a681885bb260b860ef53d35091
KarlaSofiaGonzalez <sofia.gr1602@gmail.com>
2021-03-27 01:57:23 +0000

    Clockwall
    Signed-off-by: KarlaSofiaGonzalez <sofia.gr1602@gmail.com>

--- labs/clockwall/clockServer.go
+++ labs/clockwall/clockServer.go
@@ -2,34 +2,71 @@
 package main
 
 import (
+	"flag"
 	"io"
 	"log"
 	"net"
+	"os"
 	"time"
+	"fmt"
 )
 
-func handleConn(c net.Conn) {
+type Input struct {
+	port     int
+	timezone string
+}
+
+func handleConn(c net.Conn, timeZ string) {
 	defer c.Close()
+	locat, _ := time.LoadLocation(timeZ)
 	for {
-		_, err := io.WriteString(c, time.Now().Format("15:04:05\n"))
+		/print time in locat/
+		time_now := time.Now().In(locat).Format("15:04:05\n")
+		response := timeZ + " " + time_now
+		_, err := io.WriteString(c, response)
+
 		if err != nil {
 			return // e.g., client disconnected
 		}
-		time.Sleep(1 * time.Second)
+		time.Sleep(10 * time.Second)
 	}
 }
 
 func main() {
-	listener, err := net.Listen("tcp", "localhost:9090")
+
+	var input Input
+	var host string
+
+	input = manageInput()
+	host = "0.0.0.0:" + fmt.Sprintf("%d", input.port)
+
+	listener, err := net.Listen("tcp", host)
 	if err != nil {
 		log.Fatal(err)
 	}
+	log.Print(host)
 	for {
 		conn, err := listener.Accept()
+		log.Print(conn)
 		if err != nil {
 			log.Print(err) // e.g., connection aborted
 			continue
 		}
-		go handleConn(conn) // handle connections concurrently
+		go handleConn(conn, input.timezone) 
 	}
+}
+
+func manageInput() Input {
+
+	var input Input
+	var tp *int
+
+	input.timezone = os.Getenv("TZ")
+
+	tp = flag.Int("port", 9000, "port number.")
+	flag.Parse()
+	input.port = *tp
+
+	return input
+
 }
---
+++ labs/clockwall/clockWall.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func printTime(conn io.Reader, stdout io.Writer) {
+	_, err := io.Copy(stdout, conn)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	hosts := make([]string, 0)
+	for _, arg := range args {
+		indexEq := strings.Index(arg, "=") + 1
+		hosts = append(hosts, arg[indexEq:])
+	}
+	response := make(chan io.Reader)
+	defer close(response)
+
+	for {
+		for _, host := range hosts {
+			go getTime(response, host)
+			time := <-response
+			go printTime(time, os.Stdout)
+		}
+	}
+}
+
+func getTime(time chan io.Reader, host string) {
+	conn, err := net.Dial("tcp", host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	time <- conn
+}
